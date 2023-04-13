0021253168c09609e824af9a264cab7e88675a4b
DiegoDuenez <19170154@uttcampus.edu.mx>
2023-04-13 17:17:19 -0600

    add:

--- index/views.py
+++ index/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from docx.opc.constants import RELATIONSHIP_TYPE as RT
 # Create your views here.
 
 def index(request):
-    title = "DJANGO APP"
+
+ 
+    links = [
+
+            {
+                "title":"Extraction, Purification, Structural Characteristics, Biological Activities and Pharmacological Applications of Acemannan, a Polysaccharide from Aloe vera: A Review",
+                "url":"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6515206/"
+            },
+        
+            {
+                "title": "Chemical characterization of the immunomodulating polysaccharide of Aloe vera L",
+                "url":"https://www.researchgate.net/publication/7939131_Chemical_characterisation_of_the_immunomodulating_polysaccharide_of_Aloe_vera_L"
+            },
+
+            {
+                "title":"Oral administration of Aloe vera gel, anti-microbial and anti-inflammatory herbal remedy, stimulates cell-mediated immunity and antibody production in a mouse model",
+                "url":"https://www.termedia.pl/Experimental-immunology-Oral-administration-of-Aloe-vera-gel-anti-microbial-and-anti-inflammatory-herbal-remedy-stimulates-cell-mediated-immunity-and-antibody-production-in-a-mouse-model,10,22985,1,1.html"
+            },
+
+            {
+                "title":""
+            }
+    ]
+
     return render(request, "index.html", {
-        'title': title
+        'links': links
     })
